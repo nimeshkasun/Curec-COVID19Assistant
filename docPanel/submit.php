dc2126030ac7b3c3c3cb17448f521a370e10d31d
nimeshkasun <nimesh.ekanayaka7@gmail.com>
2020-04-05 22:13:01 +0530

    doc v2.3
    Added fuction:
    - submit diagnose data to database
    - view/hide color label according to saved records previously

--- docPanel/submit.php
+++ docPanel/submit.php
@@ -1,12 +1,47 @@
 <!-- Post doctor's data to Database -->
 		<?php
+		session_start();
+		$did = $_SESSION["DID"];
+		$mid = $_SESSION["MID"];
+
 		require_once 'dbConn.php';
-			if(isset($_POST['btnSendMoh'])){
+			/*if(isset($_POST['btnSendMoh'])){
 				$newColorCode=mysqli_real_escape_string($conn,$_POST['colCode']);
-				$docDecision=mysqli_real_escape_string($conn,$_POST['docRec']);
 				$sql = "INSERT INTO tblData(newColorCode, docDecision) VALUES ('$newColorCode', '$docDecision')";
 						
 				if(mysqli_query($conn,$sql)){
+					header('location: contact.php');
+				}
+	
+			}*/
+
+			if(isset($_POST['btnNextPat'])){
+				$docDecision=mysqli_real_escape_string($conn,$_POST['docRec']);
+				$colorCode="";
+
+				switch ($docDecision) {
+					case 'hospitalize':
+						$colorCode = "1";
+						break;
+					
+					case 'selfqrn':
+						$colorCode = "2";
+						break;
+
+					case 'shouldcont':
+						$colorCode = "3";
+						break;
+
+					default:
+						$colorCode = "3";
+						break;
+				}
+
+				$sql = "INSERT INTO diagnose(DID, MID, Comment, status) VALUES ('$did', '$mid', '$docDecision', '$colorCode')";
+				
+				if(mysqli_query($conn,$sql)){
+					header('location: contact.php');
+				}else {
 					header('location: contact.php');
 				}
 	
