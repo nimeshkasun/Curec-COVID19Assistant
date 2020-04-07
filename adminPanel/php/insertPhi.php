21fcb192418c5d54a23f7f31b352438cb0080637
Savindu Bandara <36003016+bsavindu1998@users.noreply.github.com>
2020-04-08 00:16:37 +0530

    Insert Done
    All insert parts of different users are done. Search update and delete for all users can be submit tomorrow

--- adminPanel/php/insertPhi.php
+++ adminPanel/php/insertPhi.php
@@ -1,5 +1,27 @@
 <?php
 include('DBconn.php');
 
+if (isset($_POST['insertPHI'])){
+    $id = $_POST['phiID'];
+    $fname = $_POST['firstName'];
+    $lastname = $_POST['lastName'];
+    $nic = $_POST['nic'];
+    $email = $_POST['email'];
+    $workArea = $_POST['workArea'];
+    $telephone = $_POST['telephone'];
+    $password = $_POST['password'];
+
+    //
+    $sql = "insert into `phi`(`phiNumber`, `name`, `lastName`, `nic`, `email`, `phone`, `Colony`, `password`) VALUES ('$id','$fname','$lastname','$nic','$email','$telephone','$workArea','$password')";
+    $insertResult = mysqli_query($connection,$sql);
+
+    if ($insertResult){
+        echo "<script>alert('Successfully entered')</script>";
+        echo "<script>window.open('../admin.user.html','_self')</script>;";
+    }
+    else {
+        echo "<script>alert('Duplicate entries found')</script>";
+    }
+}
 
 ?>
