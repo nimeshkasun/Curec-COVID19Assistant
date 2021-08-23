71773eb9ac90907db185a0dd7d97a8de922aff4f
Kalhara Perera <adorekasun@gmail.com>
2021-08-24 00:44:08 +0530

    CSS update to adjust Covid count bar

--- doctor/system/index.php
+++ doctor/system/index.php
@@ -213,7 +213,7 @@ include 'logincheck.php';
 				<div class="overlay"></div>
 				<div class="container">
 					<div class="row">
-						<div class="col-md-7 col-md-offset-2 col-md-push-1 counter-wrap">
+					<div class="col-md-10 col-md-offset-1 col-md-push-1 counter-wrap">
 							<div class="row">
 								<div class="col-md-3 col-sm-4 animate-box">
 									<div class="desc">
